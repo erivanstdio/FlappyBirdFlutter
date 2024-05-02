29a8c028928b3aaa7625d0a37b1de19bf3b66232
erivanstdio <erivanstdio@gmail.com>
2024-05-02 01:13:55 -0300

    adding objects, manager and one actor. Also displaying main character on screen, as well as the platform blocks and stars.

--- lib/flappy_jagger.dart
+++ lib/flappy_jagger.dart
@@ -1,11 +1,64 @@
+import 'package:flame/camera.dart';
 import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
-import 'package:flappy_bird_flutter/actors/jagger.dart';
+import 'package:flutter/material.dart';
+
+import 'actors/jagger.dart';
+import 'actors/titan_enemy.dart';
+import 'managers/segment_manager.dart';
+import 'objects/ground_block.dart';
+import 'objects/platform_block.dart';
+import 'objects/star.dart';
 
 class FlappyJaggerGame extends FlameGame {
-  
   late JaggerPlayer _jagger;
-  
+  double objectSpeed = 0.0;
+
+  void loadGameSegments(int segmentIndex, double xPositionOffset) {
+    for (final block in segments[segmentIndex]) {
+      switch (block.blockType) {
+        case GroundBlock:
+        case PlatformBlock:
+          add(PlatformBlock(
+            gridPosition: block.gridPosition,
+            xOffset: xPositionOffset,
+          ));
+        case Star:
+          world.add(
+            Star(
+              gridPosition: block.gridPosition,
+              xOffset: xPositionOffset,
+            ),
+          );
+        case TitanEnemy:
+      }
+    }
+  }
+
+  void initializeGame() {
+    // Assume that size.x < 3200
+    print(size.x);
+    final segmentsToLoad = (size.x / 640).ceil();
+
+    print(segmentsToLoad);
+    segmentsToLoad.clamp(0, segments.length);
+
+    for (var i = 0; i <= segmentsToLoad; i++) {
+      loadGameSegments(i, (640 * i).toDouble());
+    }
+
+    _jagger = JaggerPlayer(
+      position: Vector2(128, canvasSize.y - 70),
+    );
+    world.add(_jagger);
+  }
+
+  @override
+  Color backgroundColor() {
+    return const Color.fromARGB(255, 173, 223, 247);
+  }
+
   @override
   Future<void> onLoad() async {
     await images.loadAll([
@@ -18,11 +71,20 @@ class FlappyJaggerGame extends FlameGame {
       'water_enemy.png',
     ]);
 
+
+    double mapHeight = 640;
+  
     camera.viewfinder.anchor = Anchor.topLeft;
-    
-   _jagger = JaggerPlayer(
+    camera.viewport = FixedResolutionViewport(resolution: Vector2(1800, mapHeight));
+
+//    Left -  Right
+//     0   -  100
+//    Down -   Up
+
+    _jagger = JaggerPlayer(
       position: Vector2(128, canvasSize.y - 70),
     );
     world.add(_jagger);
+    initializeGame();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'flappy_jagger.dart';
+
 void main() {
-  runApp(
-    const GameWidget<FlappyJaggerGame>.controlled(
-      gameFactory: FlappyJaggerGame.new
-    )
-  );
+
+  // changing orientation for mobile
+  // WidgetsFlutterBinding.ensureInitialized();
+  // Flame.device.fullScreen();
+  // Flame.device.setLandscape();
+  runApp(const GameWidget<FlappyJaggerGame>.controlled(
+      gameFactory: FlappyJaggerGame.new));
 }
